1224ccae5a1db33135736512fde6c56ea30ec59c
Iván Corrales Solera <developer@wesovilabs.com>
2019-02-23 08:00:19 +0100

    fix: fixing comma issue

--- src/resolvers.js
+++ src/resolvers.js
@@ -24,18 +24,17 @@ export default {
         actors: async ({id}, {total}) => {
             return await listActorsForMovie(id, total)
         },
-        budget: ({budget}, {currency}) => currency === 'Euro' ? budget : budget * 1.14
-        },
+        budget: ({budget}, {currency}) => currency === 'Euro' ? budget : budget * 1.14,
         rate: async ({id}) => {
             return await getRateForMovie(id)
-        }
+        },
     },
     Query: {
         listDirectors,
         listActors,
         listMovies,
         getMovie,
-        getMovieRate,
+        getMovieRate
     },
     Mutation: {
         addMovie,
@@ -43,10 +42,10 @@ export default {
         deleteActor,
         addDirector,
         deleteDirector,
-        rateMovie,
+        rateMovie
     },
     Subscription: {
         listenDirectorMovies,
-        listenRates,
+        listenRates
     }
 }
