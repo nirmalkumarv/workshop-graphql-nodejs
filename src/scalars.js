1cf8d604a63edb98f02a39045e5f05053fd6411c
Iván Corrales Solera <ivan.corrales.solera@gmail.com>
2019-03-10 12:41:18 +0100

    feat: removing console.logs

--- src/scalars.js
+++ src/scalars.js
@@ -20,7 +20,6 @@ export const Url = new GraphQLScalarType({
 });
 
 export const validateUrl = (str) => {
-    console.log(str)
     let pattern = new RegExp('^(https?:\\/\\/)' + // protocol
         '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|' + // domain name
         '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
